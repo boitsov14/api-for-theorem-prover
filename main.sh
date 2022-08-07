9c7f1e1cf13fcc14b4b7c034244e710bcc780a81
boitsov14 <86334557+boitsov14@users.noreply.github.com>
2022-08-07 17:33:19 +0900

    main.shの修正

--- main.sh
+++ main.sh
@@ -5,22 +5,25 @@ cd workdir
 # main.jar の実行
 # heap size を300MBに制限
 # stack size を512KBに制限
-java -Xmx300m -Xss512k -XX:CICompilerCount=2 -jar ../main.jar "$ID" "$SEQUENT" > "$ID"_message.txt
+java -Xmx300m -Xss512k -XX:CICompilerCount=2 -jar ../main.jar "$ID" "$SEQUENT" >"$ID"_message.txt
 
 # ID.tex が存在しているとき
 if [[ -e "$ID".tex ]]; then
     # 標準出力を ID.log に追記し，標準エラー出力を ID_error.logに追記する
-    latex -halt-on-error "$ID".tex 1>> "$ID".log 2>> "$ID"_error.log
+    latex -halt-on-error "$ID".tex 1>>"$ID".log 2>>"$ID"_error.log
 fi
 
 # ID.dvi が存在しているとき
 if [[ -e "$ID".dvi ]]; then
     # 標準出力を ID.log に追記し，標準エラー出力を ID_error.logに追記する
-    dvipng "$ID".dvi 1>> "$ID".log 2>> "$ID"_error.log
+    dvipng "$ID".dvi 1>>"$ID".log 2>>"$ID"_error.log
 fi
 
-# ID_error.logを標準エラー出力として表示
-cat "$ID"_error.log 1>&2
+# ID_error.log が存在しているとき
+if [[ -e "$ID"_error.log ]]; then
+    # ID_error.logを標準エラー出力として表示
+    cat "$ID"_error.log 1>&2
 
-# ID_error.logをID.logに追記
-cat "$ID"_error.log >> "$ID".log
+    # ID_error.logをID.logに追記
+    cat "$ID"_error.log >>"$ID".log
+fi
