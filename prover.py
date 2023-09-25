70366f917392ba4f7eeae794d1f8f50dbb7f51e3
boitsov14 <86334557+boitsov14@users.noreply.github.com>
2023-09-25 21:26:35 +0900

    mod

--- prover.py
+++ prover.py
@@ -1,20 +1,26 @@
 import os
 import subprocess
-from subprocess import CalledProcessError, TimeoutExpired
+from subprocess import CalledProcessError
 
 from notify import notify_line
 
 
-def make_proof_tree(sequent: str, size: str, timeout: float) -> str:
-    return prove(sequent, size, timeout) + make_dvi() + make_img()
+def make_proof_tree(sequent: str, memory: str, timeout: int) -> str:
+    return prove(sequent, memory, timeout) + make_dvi() + make_img()
 
 
-def prove(sequent: str, size: str, timeout: float) -> str:
+def prove(sequent: str, memory: str, timeout: int) -> str:
     try:
-        cmd = ['./prover', '-Xmx' + size, 'out', sequent]
-        result = subprocess.run(cmd, capture_output=True, check=True, text=True, timeout=timeout)
+        cmd = ['../prover.sh', sequent, memory, str(timeout)]
+        result = subprocess.run(cmd, capture_output=True, check=True, text=True)
         return result.stdout
     except CalledProcessError as e:
+        # Timeout
+        if 'CPU time limit exceeded' in e.stderr:
+            if not e.stdout:
+                return 'Proof Failed: Timeout.'
+            else:
+                return e.stdout + ' The proof tree is too large to output: Timeout.'
         # OutOfMemoryError
         if 'OutOfMemoryError' in e.stderr:
             if not e.stdout:
@@ -29,14 +35,6 @@ def prove(sequent: str, size: str, timeout: float) -> str:
         notify_line(f'stdout: {e.stdout}')
         notify_line(f'stderr: {e.stderr}')
         return 'An unexpected error has occurred: Binary Execution Error. The bug report was sent to the developer.'
-    except TimeoutExpired as e:
-        # Timeout
-        if not e.stdout:
-            return 'Proof Failed: Timeout.'
-        else:
-            if isinstance(e.stdout, bytes):
-                e.stdout = e.stdout.decode()
-            return e.stdout + ' The proof tree is too large to output: Timeout.'
 
 
 def make_dvi() -> str:
@@ -73,7 +71,7 @@ def make_img() -> str:
             raise CalledProcessError(1, cmd, result.stdout, result.stderr)
         # 正常時
         return ''
-    except (CalledProcessError, TimeoutExpired) as e:
+    except CalledProcessError as e:
         # DVI stack overflow
         if 'DVI stack overflow' in e.stderr:
             return ' The proof tree is too large to output: DVI stack overflow.'
