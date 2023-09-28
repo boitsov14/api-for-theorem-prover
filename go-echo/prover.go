141a150a055d3eef4b7c0b6f6a523d92bb433cf0
boitsov14 <86334557+boitsov14@users.noreply.github.com>
2023-09-28 23:26:18 +0900

    fix bug

--- go-echo/prover.go
+++ go-echo/prover.go
@@ -11,10 +11,7 @@ import (
 
 func prove(sequent, memory string, timeout int) (string, error) {
 	// run the command
-	stdout, stderr, exitCode, err := runCommand("../prover.sh", sequent, memory, strconv.Itoa(timeout))
-	if err != nil {
-		return "", err
-	}
+	stdout, stderr, commandErr := runCommand("../prover.sh", sequent, memory, strconv.Itoa(timeout))
 
 	// Timeout
 	if strings.Contains(stderr, "CPU time limit exceeded") {
@@ -37,7 +34,7 @@ func prove(sequent, memory string, timeout int) (string, error) {
 		return "Proof Failed: StackOverflowError.", nil
 	}
 	// other err
-	if _, err := os.Stat("out.tex"); err != nil || exitCode != 0 {
+	if _, err := os.Stat("out.tex"); err != nil || commandErr != nil {
 		return "", fmt.Errorf("binary execution error.\n"+"stdout: %s\n"+"stderr: %s", stdout, stderr)
 	}
 	// success
@@ -51,17 +48,14 @@ func makeDVI() (string, error) {
 	}
 
 	// run the command
-	stdout, stderr, exitCode, err := runCommand("latex", "-halt-on-error", "-interaction=nonstopmode", "out.tex")
-	if err != nil {
-		return "", err
-	}
+	stdout, stderr, commandErr := runCommand("latex", "-halt-on-error", "-interaction=nonstopmode", "out.tex")
 
 	// Dimension too large
 	if strings.Contains(stdout, "Dimension too large") {
 		return " The proof tree is too large to output: Dimension too large.", nil
 	}
 	// other err
-	if _, err := os.Stat("out.dvi"); err != nil || exitCode != 0 {
+	if _, err := os.Stat("out.dvi"); err != nil || commandErr != nil {
 		return "", fmt.Errorf("could not compile tex file.\n"+"stdout: %s\n"+"stderr: %s", stdout, stderr)
 	}
 	// success
@@ -75,24 +69,21 @@ func makePNG() (string, error) {
 	}
 
 	// run the command
-	stdout, stderr, exitCode, err := runCommand("dvipng", "out.dvi", "-o", "out.png")
-	if err != nil {
-		return "", err
-	}
+	stdout, stderr, commandErr := runCommand("dvipng", "out.dvi", "-o", "out.png")
 
 	// DVI stack overflow
 	if strings.Contains(stderr, "DVI stack overflow") {
 		return " The proof tree is too large to output: DVI stack overflow.", nil
 	}
 	// other err
-	if _, err := os.Stat("out.png"); err != nil || exitCode != 0 {
+	if _, err := os.Stat("out.png"); err != nil || commandErr != nil {
 		return "", fmt.Errorf("could not compile dvi file.\n"+"stdout: %s\n"+"stderr: %s", stdout, stderr)
 	}
 	// success
 	return "", nil
 }
 
-func runCommand(name string, arg ...string) (string, string, int, error) {
+func runCommand(name string, arg ...string) (string, string, error) {
 	cmd := exec.Command(name, arg...)
 
 	// create a buffer to capture stdout and stderr
@@ -101,9 +92,7 @@ func runCommand(name string, arg ...string) (string, string, int, error) {
 	cmd.Stderr = &stderr
 
 	// run the command
-	if err := cmd.Run(); err != nil {
-		return "", "", -1, err
-	}
+	err := cmd.Run()
 
-	return stdout.String(), stderr.String(), cmd.ProcessState.ExitCode(), nil
+	return stdout.String(), stderr.String(), err
 }
